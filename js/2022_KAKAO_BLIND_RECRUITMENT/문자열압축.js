913b3d790fda99199288635d738930c338f53435
Kwon Taeyoon <tykwon97@naver.com>
2022-05-03 01:27:43 +0900

    문자열 압축 good_solution 공부 / s.length의 반만 탐색해도 가능

--- js/2022_KAKAO_BLIND_RECRUITMENT/문자열압축.js
+++ js/2022_KAKAO_BLIND_RECRUITMENT/문자열압축.js
@@ -9,7 +9,7 @@ function solution(s) {
     if(s.length == 0) return 0;
     if(s.length == 1) return 1;
 
-    for(let i=1; i<s.length; i++){ //1부터 문자열길이까지
+    for(let i=1; i<s.length/2+1; i++){ //1부터 절반까지만 체크하면됨
         count = 0;
         check = 1; 
         for(let j=0; j<s.length; j = j+i){
@@ -55,13 +55,45 @@ function solution(s) {
     return answer;
 }
 
+// 다른 사람 코드 리뷰(속도는 내가 만든 코드가 더 빠르다)
+const good_solution = s => {
+    const range = [...Array(s.length)].map((_, i) => i + 1); // s의 길이와 같은 index에 1을 더한 원소를 갖는 배열 생성
+    // console.log(range)
+    // console.log(s.length)
+    return Math.min(...range.map(i => compress(s, i).length)); // 압축한 글자의 길이를 구해서 최소값을 구한다.
+    // i 에는 1부터 s.length까지 들어감
+};
+  
+const compress = (s, n) => {
+    const make = ([a, l, c]) => `${a}${c > 1 ? c : ''}${l}`;
+    // a는 압축한 결과, l은 내가 짠 코드에서 tempStr, c는 count
+    let x = make(
+        chunk(s, n).reduce( // 조각 낸 배열이 reduce된다. reduce(누적값, 현재값) 
+            ( [a, l, c], e) => 
+              // [a, l, c]에 해당
+              e === l ? [a, l, c + 1] : [ make([a, l, c]) , e, 1 ] , // [a는 make([a,l,c], l은 e, c = 1 ]
+              // e에 해당
+              ['', '', 0]   
+          )
+    );
+    // console.log(x)
+    return x;
+};
+  
+// 문자열을 i를 기준으로 조각 내는 함수
+const chunk = (s, n) =>
+    s.length <= n ? [s] : [s.slice(0, n), ...chunk(s.slice(n), n)];
+
 // s = "aabbaccc"; //7
 // s = "ababcdcdababcdcd"; //9
 // s = "abcabcdede"; //8
 // s = "abcabcabcabcdededededede"; //14
 // s = "xababcdcdababcdcd"; //17
 
- s = "a";
+ s = "aaaaabcd";
 
-let result = solution(s);
-console.log(result);
+let result1 = solution(s);
+console.log(result1);
+
+let result2 = good_solution(s);
+console.log(result2);
