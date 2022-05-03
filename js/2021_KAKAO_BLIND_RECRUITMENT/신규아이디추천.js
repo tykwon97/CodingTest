4f58f43871137d2c5462db0912b91fb7e2ee1af8
Kwon Taeyoon <tykwon97@naver.com>
2022-05-03 21:43:45 +0900

    정규표현식을 활용한 good_solution 공부

--- js/2021_KAKAO_BLIND_RECRUITMENT/신규아이디추천.js
+++ js/2021_KAKAO_BLIND_RECRUITMENT/신규아이디추천.js
@@ -59,11 +59,35 @@ function solution(new_id) {
     return answer;
 }
 
+// 정규 표현식을 활용한 다른 분의 코드
+/* /와 /g가운데에 정규표현식을 넣으세요.
+ * g는 global의 약자로, 정규표현식과 일치하는 모든 내용을 찾아오라는 옵션입니다.
+ */
+function good_solution(new_id) {
+    const answer = new_id
+        .toLowerCase() // 1 -> 소문자로 변경
+        .replace(/ [^\w-_.] /g, '') // 2 -> 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자 제거
+        // [^] : 해당하지 않는 글자를 찾는다.
+        // \w : 영어 알파벳, 숫자, 언더스코어(_)
+        .replace(/ \.+ /g, '.') // 3 -> 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
+        // +는 "하나 혹은 그 이상 연결된"
+        .replace(/ ^\. | \.$ /g, '') // 4 -> 마침표(.)가 처음이나 끝에 위치한다면 제거
+        // ^ : 줄(Line)의 시작에서 일치
+        // $ : 줄(Line)의 끝에서 일치
+        .replace(/ ^$ /, 'a') // 5 -> 빈 문자열이라면, "a"를 대입
+        .slice(0, 15).replace(/\.$/, ''); // 6 -> 길이가 16자 이상이면,첫 15개의 문자를 제외한 나머지 문자들을 모두 제거 + 마침표(.)가 끝에 위치한다면 마침표(.) 문자를 제거
+    const len = answer.length;
+    return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len); //7 -> 길이가 2자 이하라면, 마지막 문자를 길이가 3이 될 때까지 반복해서 끝에 붙암
+}
+
 new_id = "...!@BaT#*..y.abcdefghijklm"; // "bat.y.abcdefghi"
 // new_id = "z-+.^.";// "z--"
 // new_id = "=.=";// "aaa"
 // new_id = "123_.def";// "123_.def"
 // new_id = "abcdefghijklmn.p";// "abcdefghijklmn"
 
-result = solution(new_id);
-console.log(result);
+result1 = solution(new_id);
+console.log(result1);
+
+result2 = good_solution(new_id);
+console.log(result2);
