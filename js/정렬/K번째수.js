91d355c07661362d75ce13745255f8281250d104
Kwon Taeyoon <tykwon97@naver.com>
2022-05-08 01:16:06 +0900

    K번째수 100점 (다른 사람 코드도 비슷)

---
+++ js/정렬/K번째수.js
@@ -0,0 +1,33 @@
+function solution(array, commands) {
+    let answer = [];
+    commands.forEach((command) => {
+        let [i,j,k] = command;
+        // console.log(i,j,k);
+        let arr = array.slice(i-1,j);
+        arr = arr.sort();
+        // console.log(arr);
+        answer.push(arr[k-1]);
+    })
+    return answer;
+}
+
+function other_solution(array, commands) {
+    return commands.map(command => {
+        const [sPosition, ePosition, position] = command
+        const newArray = array
+            .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
+            .sort((a,b) => a - b)    
+
+        return newArray[position - 1]
+    })
+}
+
+
+array = [1, 5, 2, 6, 3, 7, 4];
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];	
+
+let result = solution(array,commands);
+console.log(result);
+
+result = other_solution(array,commands);
+console.log(result);
