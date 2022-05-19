69c186b3405db155a19dcc05eba8ec6c2f1b3a14
Kwon Taeyoon <tykwon97@naver.com>
2022-05-19 17:44:52 +0900

    스도쿠 검사

---
+++ Python/파이썬 알고리즘/섹션 3/10 - 스토쿠 검사/ex_AA.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open('input.txt', 'rt')
+# 더하기 하는 방식이 아닌 1로 대체 하는 방식을 사용해서 sum을 사용할 수 있도록 함!!!!!
+
+
+def check(a):
+    for i in range(9):
+        ch1 = [0]*10
+        ch2 = [0]*10
+        for j in range(9):
+            ch1[a[i][j]] = 1
+            ch2[a[j][i]] = 1
+        if(sum(ch1) != 9 or sum(ch2) != 9):
+            return False
+
+    for i in range(3):
+        for j in range(3):
+            ch3 = [0]*10
+            for k in range(3):
+                for s in range(3):
+                    ch3[a[i*3+k][j*3+s]] = 1
+            if(sum(ch3) != 9):
+                False
+
+    return True
+
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
