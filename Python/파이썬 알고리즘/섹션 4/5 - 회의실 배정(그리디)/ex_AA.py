16f05ecadc06042de5fa25351e59b87676cdd029
Kwon Taeyoon <tykwon97@naver.com>
2022-05-21 00:58:09 +0900

    회의실 배정(정렬 기준 주석 추가)

--- Python/파이썬 알고리즘/섹션 4/5 - 회의실 배정(그리디)/ex_AA.py
+++ Python/파이썬 알고리즘/섹션 4/5 - 회의실 배정(그리디)/ex_AA.py
@@ -8,6 +8,9 @@ for i in range(n):
 
 meeting.sort(key=lambda x: (x[1], x[0]))
 # sort할 key값 정하기 x[1] x[0] 순으로 정렬
+# 끝나는 시간이 같을 경우이다.
+# 끝나는 시간이 같다면 빨리 시작하는 순서대로 정렬이 되어야 한다.
+
 
 et = 0  # end time
 cnt = 0
