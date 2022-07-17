548c06f1967e92ca0a5658724c948ac3f9172854
Kwon Taeyoon <tykwon97@naver.com>
2022-07-17 23:36:45 +0900

    SWEA  사전과제 2문제

---
+++ SWEA/SWEA/src/N1959_TwoStringOfNumbers.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N1959_TwoStringOfNumbers {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		
+		for(int i=0; i<num;i++) {
+			int N = input.nextInt();
+			int M = input.nextInt();
+			int[] N_arr = new int[N];
+			int[] M_arr = new int[M];
+				
+			for(int j=0; j<N; j++) {
+				N_arr[j] = input.nextInt();
+			}
+			for(int j=0; j<M; j++) {
+				M_arr[j] = input.nextInt();
+			}
+			
+//			System.out.println(Arrays.toString(N_arr));
+//			System.out.println(Arrays.toString(M_arr));
+			
+			int maxValue = 0;
+			if(N>=M) {
+				for(int j=0; j<N-M+1; j++) {
+					int temp = 0;
+					for(int k=0; k<M; k++) {
+						temp += (N_arr[j+k]*M_arr[k]);
+					}
+					
+					if(temp > maxValue) {
+						maxValue = temp;
+					}
+					
+				}
+			}else {
+				
+				for(int j=0; j<M-N+1; j++) {
+					int temp = 0;
+					for(int k=0; k<N; k++) {
+						temp += (N_arr[k]*M_arr[j+k]);
+					}
+					
+					if(temp > maxValue) {
+						maxValue = temp;
+					}
+					
+				}
+			}
+			
+			System.out.println("#" + (i+1) + " " + maxValue);
+		}
+	}
+
+}
