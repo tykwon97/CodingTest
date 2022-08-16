645ea60a5a85317af66071877df150e2cfdb599c
tykwon97 <tykwon97@naver.com>
2022-08-17 01:14:28 +0900

    BOJ - 11949/2798/1018/2839

--- java/Baekjoon/src/bronze2/N2798_Solution.java
+++ java/Baekjoon/src/bronze2/N2798_Solution.java
@@ -7,32 +7,57 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class N2798_Solution {
+	
+	public static int[] arr;
+	public static int n;
+	public static int m;
+	public static int min;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(in.readLine()," ");
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[n];
+		arr = new int[n];
 		st = new StringTokenizer(in.readLine()," ");
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		int min = 0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i+1; j < arr.length; j++) {
-				for (int k = j+1; k < arr.length; k++) {
-					int total = arr[i] + arr[j] + arr[k];
-					if(m >= total && (m-min) > (m-total)) {
-						
-						min = total;
-					}
-				}
-			}
-		}
+		
+		// 재귀로 풀어보기
+		comb(0,0,0);
+		
+//		for (int i = 0; i < arr.length; i++) {
+//			for (int j = i+1; j < arr.length; j++) {
+//				for (int k = j+1; k < arr.length; k++) {
+//					int total = arr[i] + arr[j] + arr[k];
+//					if(m >= total && (m-min) > (m-total)) {
+//						
+//						min = total;
+//					}
+//				}
+//			}
+//		}
 		
 		System.out.println(min);
+	}
+
+	private static void comb(int total, int count, int index) {
+		
+		if(total>m) return;
+		if(count == 3) {
+			if(m >= total && total > min) {
+				min = total;
+			}
+			return;
+		}
+		
+		for (int i = index; i < n; i++) {
+			comb(total+arr[i], count+1, i+1);
+		}
+		
 	}
 }
 
