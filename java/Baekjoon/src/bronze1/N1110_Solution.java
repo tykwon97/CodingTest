b75e5b546f7d0cd15deb808458115c9eb4c79475
tykwon97 <tykwon97@naver.com>
2022-09-01 22:55:46 +0900

    BOJ - 1110

---
+++ java/Baekjoon/src/bronze1/N1110_Solution.java
@@ -0,0 +1,25 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class N1110_Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(in.readLine());
+		int count = 0;
+		int x = num;
+		do {
+			
+			int a = x/10%10;
+			int b = x%10;
+			x = b*10+(a+b)%10;
+			count+=1;
+//			System.out.println("a : "+a+ " b : "+b+" x : "+x+" count : "+count);
+			
+		}while(x != num);
+		System.out.println(count);
+	}
+
+}
