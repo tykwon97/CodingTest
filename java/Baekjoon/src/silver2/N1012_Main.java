bc81e5d4eea65c829b1d3967109350b270c9e36d
tykwon97 <tykwon97@naver.com>
2022-10-18 00:13:58 +0900

    BOJ - 15961/1012/1209/10819/11048

--- java/Baekjoon/src/silver2/N1012_Main.java
+++ java/Baekjoon/src/silver2/N1012_Main.java
@@ -10,93 +10,86 @@ public class N1012_Main {
 	public static int M;
 	public static int N;
 	public static int[][] root;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+
 		int T = Integer.parseInt(in.readLine());
 		for (int i = 0; i < T; i++) {
 			st = new StringTokenizer(in.readLine(), " ");
 			M = Integer.parseInt(st.nextToken());
 			N = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
-			
+
 			boolean[][] map = new boolean[N][M];
-			boolean[][] isSelected = new boolean[N][M];
 			for (int j = 0; j < K; j++) {
 				st = new StringTokenizer(in.readLine(), " ");
 				int y = Integer.parseInt(st.nextToken());
 				int x = Integer.parseInt(st.nextToken());
-				map[x][y] = true;
+				map[x][y] = true; // 배추 표시
 			}
-			
+
 			root = new int[N][M]; //초기화 작업
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < M; k++) {
-					if(map[j][k])
-						root[j][k] = j*M+k;
+			for (int x = 0; x < N; x++) {
+				for (int y = 0; y < M; y++) {
+					if(map[x][y]) // 배추있는 곳만 초기화
+						root[x][y] = x*M+y;
 					else
-						root[j][k] = N*M;
+						root[x][y] = N*M;
 				}
 			}
-			
+
 			int[] dx = {1,0,0,-1};
 			int[] dy = {0,1,-1,0};
 			for (int x = 0; x < N; x++) {
 				for (int y = 0; y < M; y++) {
-					if(map[x][y]) {
-						for (int l = 0; l < 4; l++) {
+					if(map[x][y]) { // 배추일때 (x,y)기준으로 4방탐색
+						for (int l = 0; l < 4; l++) { 
 							int newX = x+dx[l];
 							int newY = y+dy[l];
-							if(newX<0 || newY<0 || newX>=N || newY>=M)
+							if(newX<0 || newY<0 || newX>=N || newY>=M) //범위 벗어난 경우
 								continue;
-							if(map[newX][newY]) {
-								isSelected[x][y] = true;
-								if(!isSelected[newX][newY])
-									make_set(x,y,newX,newY);
-//								System.out.println("X : "+ x+" y : "+y+" nx : "+newX+" ny : "+ newY);
+							if(map[newX][newY]) { //탐색된 지역에 배추가 있는 경우 Union-Find
+								make_set(x,y,newX,newY);
 							}
 						}
 					}
 				}
 			}
 
-			HashSet<Integer> set = new HashSet<>();
+			HashSet<Integer> set = new HashSet<>(); // Union-Find 개수 구하기
 			for (int x = 0; x < N; x++) {
 				for (int y = 0; y < M; y++) {
-//					System.out.print(root[x][y]+" ");
-					set.add(root[x][y]);
+					if(map[x][y]) { //배추 있는 부분만
+						int rootValue = find(x,y); // 루트 노드 찾기
+						int rootX = rootValue/M;
+						int rootY = rootValue%M;
+						set.add(root[rootX][rootY]);
+					}
 				}
 			}
-			int size = set.size()-1;
-			if(size == 0)
-				sb.append(set.size()).append("\n");
-			else
-				sb.append(set.size()-1).append("\n");
+
+			int size = set.size();
+			sb.append(size).append("\n");
 		}
+
 		System.out.println(sb);
 	}
-	
+
 
 	private static void make_set(int x1, int y1, int x2, int y2) {
 		int parent1 = find(x1,y1);
 		int parent2 = find(x2,y2);
-//		System.out.println("X : "+ x1+" y : "+y1+" y1 : "+x2+" ny : "+ y2);
-//		System.out.println("parnet 1 : "+parent1+"parent2 : "+parent2);
+		
 		if(parent1 == parent2) {
 			return;
 		}else {
-			if(parent1> parent2)
-				root[x1][y1] = parent2;
-			else
-				root[x2][y2] = parent1;
-				
+			root[parent1/M][parent1%M] = parent2; // 루트 노드만 변경
+			find(x1,y1); // 루트 노드부터 차례대로 변경해주기
 		}
-		
 	}
-
 
 	private static int find(int x, int y) {
 		int parent = root[x][y];
@@ -107,9 +100,5 @@ public class N1012_Main {
 			int nextY = parent%M;
 			return root[x][y] = find(nextX,nextY);
 		}
-		
 	} 
-
-	
-
 }
