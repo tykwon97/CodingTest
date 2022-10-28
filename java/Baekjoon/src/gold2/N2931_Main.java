ea6714f0796c54b562d003a02aae3f6df3f91171
tykwon97 <tykwon97@naver.com>
2022-10-28 16:11:07 +0900

    BOJ- 2931/1325

--- java/Baekjoon/src/gold2/N2931_Main.java
+++ java/Baekjoon/src/gold2/N2931_Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -21,25 +20,24 @@ public class N2931_Main {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
-
 		st = new StringTokenizer(in.readLine(), " ");
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
 		map = new char[R][C]; // 통로
 
-		int[] spot = new int[4];
-		int index = 0; //spot에 저장할 위치
-
 		for (int j = 0; j < R; j++) { // 통로 입력받기
 			map[j] = in.readLine().toCharArray();
 		}
+		
+		int spotX = -1, spotY = -1;
 
 		for (int j = 0; j < R; j++) {
 			for (int k = 0; k < C; k++) {
-				if('M'==map[j][k] || 'Z'==map[j][k]){ // 집과 유치원 위치 x,y순서로 저장
-					spot[index++] = j;
-					spot[index++] = k;
+				if('M'==map[j][k] || 'Z'==map[j][k]){ // 집과 유치원 위치 중 하나 저장
+					spotX = j;
+					spotY = k;
+					break;
 				}
 			}
 		}
@@ -47,12 +45,11 @@ public class N2931_Main {
 		// 지워진 위치
 		roadX = -1;
 		roadY = -1;
+		
 		// 지워진 그림
 		operation = "$";
 
-		for (int j = 0; j < 2; j++) {
-			bfs(spot[j*2],spot[j*2+1]);
-		}
+		bfs(spotX,spotY);
 
 		sb.append(roadX+1).append(" ").append(roadY+1).append(" ").append(operation).append("\n");
 
@@ -62,9 +59,6 @@ public class N2931_Main {
 	private static void bfs(int startX, int startY) {
 		Queue<int[]> queue = new ArrayDeque<>();
 
-		boolean[][] isSelected = new boolean[R][C];
-		isSelected[startX][startY] = true; // 이중 방문 예방
-
 		int[] dx = {-1,+0,+1,+0};
 		int[] dy = {+0,+1,+0,-1};
 
@@ -72,32 +66,80 @@ public class N2931_Main {
 		for (int i = 0; i < 4; i++) { 
 			int nx = startX+dx[i];
 			int ny = startY+dy[i];
+			
 			if(nx<0 || ny<0 || nx>=R || ny>=C) //범위를 벗어난 경우
 				continue;
-			if(map[nx][ny] == 'M' || map[nx][ny] == 'Z') //통로가 없는 경우
+			
+			if(map[nx][ny] == 'M' || map[nx][ny] == 'Z' || map[nx][ny] == '.') //통로가 아닌 경우
 				continue;
-			//			System.out.println("nx : "+nx+" ny : "+ny);
+			
 			queue.add(new int[] {nx,ny,startX,startY});
 		}
 
 		while(!queue.isEmpty()) {
+			
 			int[] xy = queue.poll();
+			
 			int x = xy[0];
 			int y = xy[1];
+			
 			int prevx = xy[2];
 			int prevy = xy[3];
-			//			System.out.println("x : "+x +" y : "+y);
-			if(isSelected[x][y]) //이미 방문한 경우
-				continue;
-			if(map[x][y] != '+')
-				isSelected[x][y] = true;
-
+			
 			char op = map[x][y];
-//			System.out.println(" x: "+x +" y: "+y+"  prevx : "+prevx+" prevy : "+prevy);
+			
 			switch (op+"") {
+			
 			case ".":
-				map[x][y] = '#'; //방문 처리
-				break;
+				
+				// 지워진 위치 도착
+				roadX = x;
+				roadY = y;
+				
+				// 통로가 연결될 위치 확인
+				boolean[] spotCheck = new boolean[4]; //위 오 아 왼
+
+				for (int i = 0; i < 4; i++) {
+					int nx = x+dx[i];
+					int ny = y+dy[i];
+					
+					if(nx<0 || ny<0 || nx>=R || ny>=C)
+						continue;
+					
+					if((map[nx][ny]=='#') || (map[nx][ny]=='.') || (map[nx][ny]=='M') || (map[nx][ny]=='Z'))  // 통로가 아닌 경우
+						continue;
+					
+					// 가능한 경우의 수 (현재 위치와 가스관 방향)
+					if(i==0 && (map[nx][ny]=='-' || map[nx][ny]=='2' || map[nx][ny]=='3')) {
+						continue;
+					}else if(i==1 && (map[nx][ny]=='|' || map[nx][ny]=='1' || map[nx][ny]=='2')) {
+						continue;
+					}else if(i==2 && (map[nx][ny]=='-' || map[nx][ny]=='1' || map[nx][ny]=='4')) {
+						continue;
+					}else if(i==3 && (map[nx][ny]=='|' || map[nx][ny]=='3' || map[nx][ny]=='4')) {
+						continue;
+					}
+					
+					spotCheck[i] = true; // 연결 할 통로가 있는 경우
+				}
+				
+				// 알맞은 통로 찾기
+				if(spotCheck[0] && spotCheck[1] && spotCheck[2] && spotCheck[3])
+					operation = "+";
+				else if(spotCheck[0] && spotCheck[1])
+					operation = "2";
+				else if(spotCheck[0] && spotCheck[2])
+					operation = "|";
+				else if(spotCheck[0] && spotCheck[3])
+					operation = "3";
+				else if(spotCheck[1] && spotCheck[2])
+					operation = "1";
+				else if(spotCheck[1] && spotCheck[3])
+					operation = "-";
+				else if(spotCheck[2] && spotCheck[3])
+					operation = "4";
+				return;
+				
 			case "|":
 				if(prevx == x-1 && x+1<R)
 					queue.add(new int[] {x+1,y,x,y});
@@ -127,7 +169,6 @@ public class N2931_Main {
 					queue.add(new int[] {x,y+1,x,y});
 				break;
 			case "3":
-				//				System.out.println("prevx : "+prevx+" prevy : "+prevy +" x: "+x +" y: "+y);
 				if(prevy == y-1 && x-1>=0 )
 					queue.add(new int[] {x-1,y,x,y});
 				if(prevx == x-1 && y-1>=0)
@@ -145,85 +186,9 @@ public class N2931_Main {
 				if(prevx == x+1  && y-1>=0)
 					queue.add(new int[] {x,y-1,x,y});
 				break;
-			default: // 출발지에서 방문한 지역 중 도착지에서도 방문한 경우 ('#')
-				// 지워진 위치 저장
-				roadX = x;
-				roadY = y;
-//				System.out.println("x : "+x+" y : "+y);
-//				System.out.println();
-//				for (int i = 0; i < R; i++) {
-//					for (int j = 0; j < C; j++) {
-//						System.out.print(map[i][j]+" ");
-//					}System.out.println();
-//				}
-				// 통로가 연결될 위치 확인
-				boolean[] spotCheck = new boolean[4]; //위 오 아 왼
-				boolean[] roadCheck = new boolean[4]; //위 오 아 왼
-
-				for (int i = 0; i < 4; i++) {
-					int nx = x+dx[i];
-					int ny = y+dy[i];
-					if(nx<0 || ny<0 || nx>=R || ny>=C)
-						continue;
-					if((map[nx][ny]=='#') || (map[nx][ny]=='.'))  // 통로가 아닌 경우
-						continue;
-					if((map[nx][ny]=='M') || (map[nx][ny]=='Z')) {
-						roadCheck[i] = true;
-						continue;
-					}
-					if(i==0 && (map[nx][ny]=='-' || map[nx][ny]=='2' || map[nx][ny]=='3')) {
-						continue;
-					}else if(i==1 && (map[nx][ny]=='|' || map[nx][ny]=='1' || map[nx][ny]=='2')) {
-						continue;
-					}else if(i==2 && (map[nx][ny]=='-' || map[nx][ny]=='1' || map[nx][ny]=='4')) {
-						continue;
-					}else if(i==3 && (map[nx][ny]=='|' || map[nx][ny]=='3' || map[nx][ny]=='4')) {
-						continue;
-					}
-					spotCheck[i] = true; // 연결 할 통로가 있는 경우
-				}
-//				System.out.println(Arrays.toString(spotCheck));
-				// 알맞은 통로 찾기
-				if(spotCheck[0] && spotCheck[1] && spotCheck[2] && spotCheck[3])
-					operation = "+";
-				else if(spotCheck[0] && spotCheck[1])
-					operation = "2";
-				else if(spotCheck[0] && spotCheck[2])
-					operation = "|";
-				else if(spotCheck[0] && spotCheck[3])
-					operation = "3";
-				else if(spotCheck[1] && spotCheck[2])
-					operation = "1";
-				else if(spotCheck[1] && spotCheck[3])
-					operation = "-";
-				else if(spotCheck[2] && spotCheck[3])
-					operation = "4";
-				else {
-					for (int i = 0; i < 4; i++) {
-						roadCheck[i] = roadCheck[i] || spotCheck[i];
-					}
-					if(roadCheck[0] && roadCheck[1])
-						operation = "2";
-					else if(roadCheck[0] && roadCheck[2])
-						operation = "|";
-					else if(roadCheck[0] && roadCheck[3])
-						operation = "3";
-					else if(roadCheck[1] && roadCheck[2])
-						operation = "1";
-					else if(roadCheck[1] && roadCheck[3])
-						operation = "-";
-					else if(roadCheck[2] && roadCheck[3])
-						operation = "4";
-				}
-				return;
+			default: 
+				break;
 			}
 		}
-
-//		System.out.println();
-//		for (int i = 0; i < R; i++) {
-//			for (int j = 0; j < C; j++) {
-//				System.out.print(map[i][j]+" ");
-//			}System.out.println();
-//		}
 	}
 }
--- java/Baekjoon/src/silver1/N1325_Main.java
+++ java/Baekjoon/src/silver1/N1325_Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,9 +14,8 @@ import java.util.StringTokenizer;
 public class N1325_Main {
 	public static int N;
 	public static int maxValue;
-	public static int[] hacking;
 	public static List<Integer> list; //결과 저장
-	public static List<List<Integer>> map; // 신뢰 관계
+	public static List<Integer>[] map; // 신뢰 관계
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -25,69 +25,81 @@ public class N1325_Main {
 		st = new StringTokenizer(in.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		boolean[] check = new boolean[N+1];
 		
-		map = new LinkedList<>();
+		map = new List[N+1];
 		for (int i = 0; i < N+1; i++) {
-			List<Integer> nList = new LinkedList<>();
-			map.add(nList);
+			map[i] = new ArrayList<Integer>();
 		}
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(in.readLine(), " ");
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
-			check[A] = true;
-			check[B] = false;
-			map.get(A).add(B);
+			map[B].add(A);
 		}
-
+		
 		list = new LinkedList<>();
-		maxValue = Integer.MIN_VALUE;
-		hacking = new int[N+1];
-		Arrays.fill(hacking, Integer.MIN_VALUE);
 		
 		for (int i = 1; i <= N; i++) {
-			if(!check[i])
-				continue;
-			BFS(i);
-		}
-		
-		for (int i = 0; i < hacking.length; i++) {
-			if(maxValue < hacking[i]) {
-				maxValue = hacking[i];
-				list = new LinkedList<>();
+//			System.out.println(i+"번째");
+			int result = BFS(i);
+			if(maxValue<result) {
+				maxValue = result;
+				list = new LinkedList<>(); 
 				list.add(i);
-			}else if(maxValue == hacking[i]) {
+			}else if(maxValue == result){
 				list.add(i);
 			}
 		}
+
+//		System.out.println(Arrays.toString(DP));	
+		for (int i : list) 
+			sb.append(i).append(" ");
 		
-		for (int i = 0; i < list.size(); i++) {
-			sb.append(list.get(i)).append(" ");
-		}
 		System.out.println(sb);
 	}
 
-	private static void BFS(int i) {
-		Queue<int[]> queue = new ArrayDeque<int[]>();
-		queue.add(new int[] {i,1});
+	private static int BFS(int start) {
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] {start,1});
+		
+		boolean[] isSelected = new boolean[N+1];
+		
+		int value = -1;
+		
+		int[] DP = new int[N+1];
+		Arrays.fill(DP, Integer.MIN_VALUE);
 		
 		while(!queue.isEmpty()) {
 			int[] arr = queue.poll();
 			int index = arr[0];
 			int cnt = arr[1];
 			
-			if(hacking[index] >= cnt) 
+//			System.out.println("index : "+index+" cnt : "+cnt+" DP[index] : "+DP[index]);
+			
+			if(isSelected[index])
 				continue;
+			isSelected[index] = true;
 			
-			hacking[index] = cnt;
+			if(DP[index] > cnt) 
+				continue;
+			DP[index] = cnt;
 			
-			List<Integer> newList = map.get(index);
-			for (int j = 0; j < newList.size(); j++) {
-				int newIndex = newList.get(j);
-				queue.add(new int[] {newIndex, cnt+1});
-			}	
+			if(cnt>value) 
+				value = cnt;
+			
+			List<Integer> newList = map[index];
+			
+//			System.out.println("==");
+			
+			for (int i  : newList) {
+//				System.out.println("+++=");
+				if(!isSelected[i]) 
+					queue.add(new int[] {i,cnt+1});
+			}
+		
 		}
-	}
+		
+		return value;
+	}	
 }
