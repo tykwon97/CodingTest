2c20d3c00bd2003d550bb5167a7126dcaa68aa96
Taeyoon Kwon <tykwon97@naver.com>
2024-06-22 19:00:16 +0900

    Delete Python directory

--- Python/Silver2/Silver 2 - 11724번 [연결 요소의 개수].py
+++
@@ -1,5 +0,0 @@
-n, m = map(int, input().split())
-
-arr = [map(lambda x: int(x), input()) for _ in range(n)]
-
-print(arr)
--- Python/미분류/1966 - 프린트 큐.py
+++
@@ -1,6 +0,0 @@
-import heapq as hq
-
-n = int(input())
-
-for _ in range(n):
-    print("what")
